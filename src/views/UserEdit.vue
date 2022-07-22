8143bc156b879a1058305a0142ec17893cdf1fed
Jomel <jajulom@gmail.com>
2022-07-22 11:41:00 +0800

    Update UserEdit.vue

--- src/views/UserEdit.vue
+++ src/views/UserEdit.vue
@@ -2,9 +2,16 @@
 <div class="w-full flex items-center justify-center min-h-screen bg-gray-100">
     <div class="px-8 py-2 mx-4 mt-4 text-left bg-white shadow-lg md:w-2/3 lg:w-2/3 sm:w-4/5 w-11/12">
         <h3 class="text-2xl font-bold text-center">Edit/Update User</h3>
-        <form action="/" id="newUserForm">
+        <div>
+            <p class="text-green-600 text-sm pb-2" v-if="successMessage">{{ successMessage }}</p>
+            <p class="text-red-500 text-sm pb-2" v-if="errorMessage">{{ errorMessage }}</p>
+        </div>
+        <form action="/" id="editUserForm">
             <div class="mt-4">
                 <div>
+                    <div v-if="errors">
+                        <p class="text-red-500 text-sm" v-if="errors.name">{{ errors.name[0] }}</p>
+                    </div>
                     <label class="block" for="Name">Name</label>
                             <input type="text" placeholder="Name"
                                 class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
@@ -13,14 +20,26 @@
                                 >
                 </div>
                 <div class="mt-4">
+                    <div v-if="errors">
+                        <p class="text-red-500 text-sm" v-if="errors.email">{{ errors.email[0] }}</p>
+                    </div>
                     <label class="block" for="email">Email</label>
-                            <input type="text" placeholder="Email"
+                            <input type="hidden" placeholder="Email"
                                 class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
                                 name="email"
                                 v-model="formData.email"
                                 >
+                                <input type="text"
+                                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                name="placeholder-email"
+                                v-model="formData.email"
+                                readonly
+                                >
                 </div>
                 <div class="mt-4">
+                    <div v-if="errors">
+                        <p class="text-red-500 text-sm" v-if="errors.password">{{ errors.password[0] }}</p>
+                    </div>
                     <label class="block">Password</label>
                             <input type="password" placeholder="Password"
                                 class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
@@ -75,6 +94,9 @@ export default {
                 'role_id': ''
             },
             user_id_edit: localStorage.getItem('user_id_edit'),
+            errors: [],
+            errorMessage: '',
+            successMessage: ''
         }
     },
     created(){
@@ -88,11 +110,12 @@ export default {
                     }
                 })
                 .then(res =>  {
-                    console.log(res.data)
+                    this.successMessage = res.data.message
                     return this.$router.push('/users')
                 })
                 .catch(err => {
-                    console.log(err.message,err)
+                     this.errors = err.response.data.errors
+                    this.errorMessage = err.response.data.message
                 })
         },
         fetchUser(){
@@ -107,7 +130,6 @@ export default {
                     this.formData.name = res.data.user.name
                     this.formData.email = res.data.user.email
                     this.formData.role_id = res.data.user.role_id
-                    console.log(res.data)
                 })
                 .catch(err => {
                     console.log(err)
